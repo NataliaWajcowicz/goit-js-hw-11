2702bcd53ef17a13333320797fe6991db5ca4bbb
NataliaWajcowicz <n.wajcowicz@o2.pl>
2022-12-16 00:09:41 +0100

    updated css
    + fix btn load-more

--- src/index.js
+++ src/index.js
@@ -18,11 +18,12 @@ let gallerySimpleLightbox = new SimpleLightbox('.gallery a');
 searchBtn.addEventListener("click", (event) => {
     event.preventDefault();
     gallery.innerHTML = '';
-    pageNumber = 1;
+  pageNumber = 1;
+  loadBtn.removeAttribute('disabled');
     loadBtn.style.display = 'none';
     searchInput = input.value.trim();
     if (searchInput.length !== 0) {
-        fetchImages(searchInput, pageNumber).then(response => {
+      fetchImages(searchInput, pageNumber).then(response => {
             if (response.hits.length === 0) {
                 Notify.failure("Sorry, there are no images matching your search query. Please try again.")
             } else {
@@ -49,31 +50,23 @@ loadBtn.addEventListener("click", () => {
         if (response.hits.length < 40) {
             loadBtn.setAttribute('disabled', true);
 
-            window.addEventListener('scroll', () => {
-                const offset = window.innerHeight + window.pageYOffset;
-                if (offset >= document.body.scrollHeight) {
-
-                    Notify.info(
-                        "We're sorry, but you've reached the end of search results."
-                    );
-                }
-            })
-                
-               
-        }
+          window.addEventListener('scroll', scrollHandler)                 
+      }
+      
     })
         
         .catch(error => {
             Notify.failure(`Sorry, something went wrong`);
         });
-    })
+})
+    
                 
                            
                    
 function createGallery(images) {
   const markup = images
     .map(image => {
-        return `<li class=gallery-list__item><div class="photo-card">
+        return `<li class="gallery__item"><div class="photo-card">
         <a class="photo-card__link" href=${image.largeImageURL}>
   <img class="photo-card__image" src="${image.webformatURL}" alt="${image.tags}" loading="lazy" /></a>
   <div class="info">
@@ -97,3 +90,14 @@ function createGallery(images) {
   gallery.innerHTML += markup;
 }
 
+function scrollHandler() {
+   const offset = window.innerHeight + window.pageYOffset;
+   if (offset >= document.body.scrollHeight) {
+      Notify.info(
+       "We're sorry, but you've reached the end of search results."
+     );
+
+       window.removeEventListener('scroll', scrollHandler);
+     
+  }  
+}
