d9ebe1e7ddf720d3a488f762c5020b97298f0f5d
NataliaWajcowicz <n.wajcowicz@o2.pl>
2022-12-19 23:25:15 +0100

    update

--- src/index.js
+++ src/index.js
@@ -1,72 +1,69 @@
-import {fetchImages} from './fetchImages'
+import { fetchImages } from './fetchImages';
 import { Notify } from 'notiflix';
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-const input = document.querySelector("[name=searchQuery]");
-const searchBtn = document.querySelector(".search-form__button");
-const gallery = document.querySelector(".gallery");
-const loadBtn = document.querySelector(".load-more");
-
-loadBtn.style.display = "none";
-
+const input = document.querySelector('[name=searchQuery]');
+const searchBtn = document.querySelector('.search-form__button');
+const gallery = document.querySelector('.gallery');
+const loadBtn = document.querySelector('.load-more');
 
 let pageNumber = 1;
 let searchInput = '';
 let gallerySimpleLightbox = new SimpleLightbox('.gallery a');
 
-searchBtn.addEventListener("click", (event) => {
-    event.preventDefault();
-    gallery.innerHTML = '';
+searchBtn.addEventListener('click', event => {
+  event.preventDefault();
+  gallery.innerHTML = '';
   pageNumber = 1;
   loadBtn.removeAttribute('disabled');
-    loadBtn.style.display = 'none';
-    searchInput = input.value.trim();
-    if (searchInput.length !== 0) {
-      fetchImages(searchInput, pageNumber).then(response => {
-            if (response.hits.length === 0) {
-                Notify.failure("Sorry, there are no images matching your search query. Please try again.")
-            } else {
-                createGallery(response.hits);
-                Notify.success(`Hooray! We found ${response.totalHits} images.`);
-                if (response.totalHits > 40) {
-                   loadBtn.style.display = 'block'; 
-                }  
-                gallerySimpleLightbox.refresh();
-            }   
-        })
-        .catch(error => {
-            Notify.failure(`Sorry, something went wrong`);
-        }) 
-        
-    }
-})
+  loadBtn.style.display = 'none';
+  searchInput = input.value.trim();
+  if (searchInput.length !== 0) {
+    fetchImages(searchInput, pageNumber)
+      .then(response => {
+        if (response.hits.length === 0) {
+          Notify.failure(
+            'Sorry, there are no images matching your search query. Please try again.'
+          );
+        } else {
+          createGallery(response.hits);
+          Notify.success(`Hooray! We found ${response.totalHits} images.`);
+          upBtn.style.display = 'block';
+          if (response.totalHits > 40) {
+            loadBtn.style.display = 'block';
+          }
+          gallerySimpleLightbox.refresh();
+        }
+      })
+      .catch(error => {
+        Notify.failure(`Sorry, something went wrong`);
+      });
+  }
+});
 
-loadBtn.addEventListener("click", () => {
-    pageNumber++;
-    searchInput = input.value.trim();
-    fetchImages(searchInput, pageNumber).then(response => {
-        createGallery(response.hits)
-        if (response.hits.length < 40) {
-            loadBtn.setAttribute('disabled', true);
+loadBtn.addEventListener('click', () => {
+  pageNumber++;
+  searchInput = input.value.trim();
+  fetchImages(searchInput, pageNumber)
+    .then(response => {
+      createGallery(response.hits);
+      if (response.hits.length < 40) {
+        loadBtn.setAttribute('disabled', true);
 
-          window.addEventListener('scroll', scrollHandler)                 
+        window.addEventListener('scroll', scrollHandler);
       }
-      
     })
-        
-        .catch(error => {
-            Notify.failure(`Sorry, something went wrong`);
-        });
-})
-    
-                
-                           
-                   
+
+    .catch(error => {
+      Notify.failure(`Sorry, something went wrong`);
+    });
+});
+
 function createGallery(images) {
   const markup = images
     .map(image => {
-        return `<li class="gallery__item"><div class="photo-card">
+      return `<li class="gallery__item"><div class="photo-card">
         <a class="photo-card__link" href=${image.largeImageURL}>
   <img class="photo-card__image" src="${image.webformatURL}" alt="${image.tags}" loading="lazy" /></a>
   <div class="info">
@@ -91,13 +88,10 @@ function createGallery(images) {
 }
 
 function scrollHandler() {
-   const offset = window.innerHeight + window.pageYOffset;
-   if (offset >= document.body.scrollHeight) {
-      Notify.info(
-       "We're sorry, but you've reached the end of search results."
-     );
+  const offset = window.innerHeight + window.pageYOffset;
+  if (offset >= document.body.scrollHeight) {
+    Notify.info("We're sorry, but you've reached the end of search results.");
 
-       window.removeEventListener('scroll', scrollHandler);
-     
-  }  
+    window.removeEventListener('scroll', scrollHandler);
+  }
 }
